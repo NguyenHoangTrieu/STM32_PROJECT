c933a075f757933a2b105c05f304740fa9fdadd7
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-18 20:09:36 +0700

    Update main.cpp

--- Using_Mbed_Tools/blinky/main.cpp
+++ Using_Mbed_Tools/blinky/main.cpp
@@ -6,6 +6,9 @@ DigitalOut led3(PD_13); // LED cam
 DigitalOut led4(PD_14); // LED đỏ
 DigitalOut led5(PD_15); // LED xanh dương
 
+// UART1 trên chân PA_15 (TX), PB_7 (RX)
+UnbufferedSerial uart1(PA_15, PB_7, 115200);  // TX, RX, Baudrate
+
 // Hàm nhấp nháy LED
 void blink_led(DigitalOut& led, int interval_ms) {
     while (true) {
@@ -14,14 +17,30 @@ void blink_led(DigitalOut& led, int interval_ms) {
     }
 }
 
-int main() {
-    Thread t1, t2, t3, t4;
+// Hàm in chuỗi "hello_world" mỗi 1 giây
+void print_hello() {
+    const char* msg = "hello_world\n";
+    while (true) {
+        uart1.write(msg, strlen(msg));
+        ThisThread::sleep_for(1s);
+    }
+}
 
-    // Dùng lambda để gói tham số
+int main() {
+    // Tạo thread với các mức ưu tiên
+    Thread t1(osPriority(1));
+    Thread t2(osPriority(1));
+    Thread t3(osPriority(1));
+    Thread t4(osPriority(1));
+    Thread t_hello(osPriority(2));  // Thread in có ưu tiên cao hơn
+
+    // Khởi động các thread
     t1.start([&]() { blink_led(led2, 200); });
     t2.start([&]() { blink_led(led3, 400); });
     t3.start([&]() { blink_led(led4, 600); });
     t4.start([&]() { blink_led(led5, 1000); });
+
+    t_hello.start(print_hello);  // Thread in UART
 
     while (true) {
         ThisThread::sleep_for(1s);
