be6351a32350afce98d4bf4a7b7ebeebb703be8e
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-18 22:12:04 +0700

    Update main.cpp

--- Using_Mbed_Tools/blinky/main.cpp
+++ Using_Mbed_Tools/blinky/main.cpp
@@ -4,7 +4,7 @@
 DigitalOut led2(PD_12); // LED xanh lá
 DigitalOut led3(PD_13); // LED cam
 // UART1 trên chân PA_15 (TX), PB_7 (RX) 
-BufferSerial uart1(PB_6, PB_3);  // TX, RX, Baudrate
+BufferedSerial uart1(PB_6, PB_3);  // TX, RX, Baudrate
 
 // Hàm nhấp nháy LED
 void blink_led(DigitalOut& led, int interval_ms) {
