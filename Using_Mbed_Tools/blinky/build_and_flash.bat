72474bffc7945cb87ebf3e56d86f1cd2b079e620
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-18 12:45:56 +0700

    Update build_and_flash.bat

--- Using_Mbed_Tools/blinky/build_and_flash.bat
+++ Using_Mbed_Tools/blinky/build_and_flash.bat
@@ -3,4 +3,3 @@ mbed-tools configure -m STM32F411_DISCOVERY -t GCC_ARM
 cd cmake_build\STM32F411_DISCOVERY\develop\GCC_ARM
 cmake -G Ninja ../../../../ (to main CmakeList)
 ninja
-STM32_Programmer_CLI -c port=SWD sn=52FF6F064849825057460467 -w blinky.bin 0x08000000 -v -rst
