32f747d4e25a4a318fc18da7f3e437febb85b733
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-18 16:15:52 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 cd Using_Mbed_Tools/blinky
 set -e
 rm -rf cmake_build
-mbed_tools configure -m STM32F411_DISCOVERY -t GCC_ARM
-cd cmake_build/STM32F411_DISCOVERY/develop/GCC_ARM
-cmake -G Ninja ../../../../
-ninja
+mbed_tools compile -m STM32F411_DISCOVERY -t GCC_ARM
