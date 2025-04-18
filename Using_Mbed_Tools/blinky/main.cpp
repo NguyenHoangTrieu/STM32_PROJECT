203d657f9e3c5b7d4c5a19ab6f1461f2f0752618
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-18 21:54:11 +0700

    Update main.cpp

--- Using_Mbed_Tools/blinky/main.cpp
+++ Using_Mbed_Tools/blinky/main.cpp
@@ -5,6 +5,7 @@ DigitalOut led2(PD_12); // LED xanh lá
 DigitalOut led3(PD_13); // LED cam
 DigitalOut led4(PD_14); // LED đỏ
 DigitalOut led5(PD_15); // LED xanh dương
+
 // UART1 trên chân PA_15 (TX), PB_7 (RX) 
 static BufferedSerial uart1(PA_15, PB_7);  // TX, RX, Baudrate
 
