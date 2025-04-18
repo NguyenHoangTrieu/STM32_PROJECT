b1dcb9c0daa8f04011aeb723b50283594e0aad91
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-18 21:17:36 +0700

    Update main.cpp

--- Using_Mbed_Tools/blinky/main.cpp
+++ Using_Mbed_Tools/blinky/main.cpp
@@ -7,7 +7,7 @@ DigitalOut led4(PD_14); // LED đỏ
 DigitalOut led5(PD_15); // LED xanh dương
 
 // UART1 trên chân PA_15 (TX), PB_7 (RX)
-//UnbufferedSerial uart1(PA_15, PB_7, 115200);  // TX, RX, Baudrate
+UnbufferedSerial uart1(PA_15, PB_7, 115200);  // TX, RX, Baudrate
 
 // Hàm nhấp nháy LED
 void blink_led(DigitalOut& led, int interval_ms) {
@@ -18,13 +18,13 @@ void blink_led(DigitalOut& led, int interval_ms) {
 }
 
 // Hàm in chuỗi "hello_world" mỗi 1 giây
-// void print_hello() {
-//     const char* msg = "hello_world\n";
-//     while (true) {
-//         uart1.write(msg, strlen(msg));
-//         ThisThread::sleep_for(1s);
-//     }
-// }
+void print_hello() {
+    const char* msg = "hello_world\n";
+    while (true) {
+        uart1.write(msg, strlen(msg));
+        ThisThread::sleep_for(1s);
+    }
+}
 
 int main() {
     // Tạo thread với các mức ưu tiên
@@ -32,7 +32,7 @@ int main() {
     Thread t2(osPriorityNormal);
     Thread t3(osPriorityNormal);
     Thread t4(osPriorityNormal);
-    //Thread t_hello(osPriorityHigh);  // Thread in có ưu tiên cao hơn
+    Thread t_hello(osPriorityHigh);  // Thread in có ưu tiên cao hơn
 
     // Khởi động các thread
     t1.start([&]() { blink_led(led2, 200); });
@@ -40,7 +40,7 @@ int main() {
     t3.start([&]() { blink_led(led4, 600); });
     t4.start([&]() { blink_led(led5, 1000); });
 
-    //t_hello.start(print_hello);  // Thread in UART
+    t_hello.start(print_hello);  // Thread in UART
 
     while (true) {
         ThisThread::sleep_for(1s);
