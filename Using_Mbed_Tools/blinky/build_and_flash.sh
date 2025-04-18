6f1e4cdd055dd43e7e5b92c4a6a9d0a206e6d05a
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-18 15:24:50 +0700

    Update build_and_flash.sh

--- Using_Mbed_Tools/blinky/build_and_flash.sh
+++ Using_Mbed_Tools/blinky/build_and_flash.sh
@@ -1,16 +1,1 @@
-set -e  # Dừng ngay khi có lỗi
 
-# Xóa thư mục cmake_build nếu tồn tại
-rm -rf cmake_build
-
-# Cấu hình project với mbed-tools
-mbed_tools configure -m STM32F411_DISCOVERY -t GCC_ARM
-
-# Di chuyển vào thư mục cấu hình
-cd cmake_build/STM32F411_DISCOVERY/develop/GCC_ARM
-
-# Gọi cmake để tạo file build với Ninja
-cmake -G Ninja ../../../../
-
-# Build project
-ninja
