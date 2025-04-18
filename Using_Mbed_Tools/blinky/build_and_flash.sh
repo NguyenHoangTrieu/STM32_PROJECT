a3a5d3e4edc5c475b0806160a6b347d8d39401e6
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-18 15:26:14 +0700

    Update build_and_flash.sh

--- Using_Mbed_Tools/blinky/build_and_flash.sh
+++ Using_Mbed_Tools/blinky/build_and_flash.sh
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 set -e
 rm -rf cmake_build
 mbed_tools configure -m STM32F411_DISCOVERY -t GCC_ARM
 cd cmake_build/STM32F411_DISCOVERY/develop/GCC_ARM
 cmake -G Ninja ../../../../
 ninja
-=======
-
->>>>>>> 6f1e4cdd055dd43e7e5b92c4a6a9d0a206e6d05a
