64780fc25e36db44ab4d544801d7939b1984b553
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-18 18:24:17 +0700

    Update build_and_flash.sh

--- Using_Mbed_Tools/blinky/build_and_flash.sh
+++ Using_Mbed_Tools/blinky/build_and_flash.sh
@@ -1,4 +1,4 @@
 set -e
 rm -rf cmake_build
 mbed_tools compile -m STM32F411_DISCOVERY -t GCC_ARM
-sudo STM32_Programmer_CLI -c port=SWD sn=52FF6E064849825043360667 -w blinky.bin 0x08000000 -v -rst
+sudo /home/npvinh/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI -c port=SWD sn=52FF6E064849825043360667 -w blinky.bin 0x08000000 -v -rst
