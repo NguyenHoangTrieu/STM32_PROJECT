af9efc1a8fc8ebe6e96804873c9fcf2af7cdf96e
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-18 13:34:15 +0700

    Update and rename build_and_flash.bat to build_and_flash.sh

--- Using_Mbed_Tools/blinky/build_and_flash.bat
+++
@@ -1,5 +0,0 @@
-rmdir /s /q cmake_build
-mbed-tools configure -m STM32F411_DISCOVERY -t GCC_ARM
-cd cmake_build\STM32F411_DISCOVERY\develop\GCC_ARM
-cmake -G Ninja ../../../../ (to main CmakeList)
-ninja
