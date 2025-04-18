418b584619e0ac7483c74c0b87e1c31d47f6ff8f
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-18 17:39:08 +0700

    Update build_and_flash.sh

--- Classic_with_CMake/Soft_Timer01/build_and_flash.sh
+++ Classic_with_CMake/Soft_Timer01/build_and_flash.sh
@@ -5,12 +5,10 @@ mkdir build
 cd build
 cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi-gcc.cmake ..
 ninja
-STM32_Programmer_CLI -c port=SWD sn=52FF6C064849825033360667 -w Example_01.bin 0x08000000 -v -rst
 cd ../../Example_02
 rm -rf build
 mkdir build
 cd build
 cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi-gcc.cmake ..
 ninja
-STM32_Programmer_CLI -c port=SWD sn=52FF6F064849825057460467 -w Example_02.bin 0x08000000 -v -rst
 echo Done!
