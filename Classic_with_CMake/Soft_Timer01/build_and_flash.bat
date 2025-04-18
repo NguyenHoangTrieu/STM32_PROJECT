7c7a329695d30688e39750486387ef8a6d4c770c
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-18 16:21:55 +0700

    Update build_and_flash.bat

--- Classic_with_CMake/Soft_Timer01/build_and_flash.bat
+++ Classic_with_CMake/Soft_Timer01/build_and_flash.bat
@@ -1,23 +1,16 @@
-@echo off
-rem Thực hiện cho Example_01
-cd Examples\Example_01
-rmdir /s /q build
+set -e
+cd Examples/Example_01
+rm -rf build
 mkdir build
 cd build
 cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi-gcc.cmake ..
 ninja
 STM32_Programmer_CLI -c port=SWD sn=52FF6C064849825033360667 -w Example_01.bin 0x08000000 -v -rst
-
-rem Quay lại thư mục gốc
-cd ..\..\Example_02
-
-rem Thực hiện cho Example_02
-rmdir /s /q build
+cd ../../Example_02
+rm -rf build
 mkdir build
 cd build
 cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi-gcc.cmake ..
 ninja
 STM32_Programmer_CLI -c port=SWD sn=52FF6F064849825057460467 -w Example_02.bin 0x08000000 -v -rst
-
-rem Kết thúc
 echo Done!
