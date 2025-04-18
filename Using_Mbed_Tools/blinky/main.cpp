8965b35b7657d6ecb2b30f0a1b94ae6eb2da9342
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-18 21:58:32 +0700

    Update main.cpp

--- Using_Mbed_Tools/blinky/main.cpp
+++ Using_Mbed_Tools/blinky/main.cpp
@@ -3,8 +3,6 @@
 // Gán LED tương ứng với chân của STM32F411 Discovery
 DigitalOut led2(PD_12); // LED xanh lá
 DigitalOut led3(PD_13); // LED cam
-DigitalOut led4(PD_14); // LED đỏ
-DigitalOut led5(PD_15); // LED xanh dương
 
 // UART1 trên chân PA_15 (TX), PB_7 (RX) 
 static BufferedSerial uart1(PA_15, PB_7);  // TX, RX, Baudrate
@@ -36,16 +34,11 @@ int main() {
     );
     Thread t1(osPriorityNormal);
     Thread t2(osPriorityNormal);
-    Thread t3(osPriorityNormal);
-    Thread t4(osPriorityNormal);
-    Thread t_hello(osPriorityHigh);  // Thread in có ưu tiên cao hơn
+    Thread t_hello(osPriorityNormal);  // Thread in có ưu tiên cao hơn
 
     // Khởi động các thread
     t1.start([&]() { blink_led(led2, 200); });
     t2.start([&]() { blink_led(led3, 400); });
-    t3.start([&]() { blink_led(led4, 600); });
-    t4.start([&]() { blink_led(led5, 1000); });
-
     t_hello.start(print_hello);  // Thread in UART
 
     while (true) {
