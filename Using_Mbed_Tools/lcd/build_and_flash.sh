59e0e5829d245a5d124b0839688fb44ace5f9c50
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-21 01:31:09 +0700

    Update

---
+++ Using_Mbed_Tools/lcd/build_and_flash.sh
@@ -0,0 +1,8 @@
+set -e
+mbed_tools deploy
+rm -rf cmake_build
+mbed_tools compile -m DISCO_F429ZI -t GCC_ARM
+cd cmake_build/DISCO_F429ZI/develop/GCC_ARM
+sudo /home/npvinh/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI -c port=SWD sn=0672FF525470514867054915 -w blinky.bin 0x08000000 -v -rst
+cd ../../../../
+pytest -s test_uart.py
---
+++ Using_Mbed_Tools/lcd/main.cpp
@@ -0,0 +1,64 @@
+#include "mbed.h"
+#include "LCD_DISCO_F429ZI.h"
+//#include "LCD_DISCO_F429ZI.h"
+DigitalOut led3(PG_13);
+DigitalOut led4(PG_14);
+BufferedSerial uart1(PB_6, PB_7);  // TX, RX
+
+Thread t3(osPriority(1));
+Thread t4(osPriority(1));
+Thread t_hello(osPriority(2));
+Thread t_hello2(osPriority(2));
+
+Ticker hello2_timer; // Timer to notify t_hello2 (Period 5s) Ticker for period, Timeout for one-shot
+
+// Thread function to blink LEDs
+void blink_led(DigitalOut& led, int interval_ms) {
+    while (true) {
+        led = !led;
+        ThisThread::sleep_for(chrono::milliseconds(interval_ms));
+    }
+}
+
+// Thread function: print hello_world every second
+void print_hello() {
+    const char* msg = "hello_world\n";
+    while (true) {
+        uart1.write(msg, strlen(msg));
+        ThisThread::sleep_for(3s);
+    }
+}
+
+// Thread function: wait for flag to print hello_world2
+void print_hello2() {
+    const char* msg = "Timer Called\n";
+    while (true) {
+        ThisThread::flags_wait_any(0x01);  // Chờ "notify"
+        uart1.write(msg, strlen(msg));
+    }
+}
+
+// Timer callback: notify t_hello2
+void hello2_notify() {
+    t_hello2.flags_set(0x01);  // Notify thread
+}
+
+int main() {
+    uart1.set_baud(115200);
+    uart1.set_format(
+        /* bits */ 8,
+        /* parity */ BufferedSerial::None,
+        /* stop bit */ 1
+    );
+
+    t3.start([&]() { blink_led(led3, 400); });
+    t4.start([&]() { blink_led(led4, 800); });
+    t_hello.start(print_hello);
+    t_hello2.start(print_hello2);
+
+    hello2_timer.attach(&hello2_notify, 5s);  // Notify mỗi 5 giây
+
+    while (true) {
+        ThisThread::sleep_for(1s);
+    }
+}
