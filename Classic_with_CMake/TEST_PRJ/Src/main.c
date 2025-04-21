240b0259fd60cfee2067a16df8b286a76b259de0
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-21 23:29:50 +0700

    Update

--- Classic_with_CMake/TEST_PRJ/Src/main.c
+++ Classic_with_CMake/TEST_PRJ/Src/main.c
@@ -121,7 +121,7 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-    HAL_UART_Transmit(&huart1, message, strlen(message), 100);
+
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -219,8 +219,8 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOG_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4, GPIO_PIN_RESET);
