e8e66ebfc19104e7e295e88a75336155cf413c84
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-21 17:24:40 +0700

    Update build_and_flash.sh

--- Using_Mbed_Tools/blinky/build_and_flash.sh
+++ Using_Mbed_Tools/blinky/build_and_flash.sh
@@ -4,5 +4,4 @@ mbed_tools compile -m DISCO_F429ZI -t GCC_ARM
 cd cmake_build/DISCO_F429ZI/develop/GCC_ARM
 sudo /home/npvinh/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI -c port=SWD sn=0672FF525470514867054915 -w blinky.bin 0x08000000 -v -rst
 cd ../../../../
-clear
 pytest -s test_uart.py
