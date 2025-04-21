08263535d1ddc1fc6b3ff4bd016e5b5c48cf8007
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-21 17:22:34 +0700

    Update build_and_flash.sh

--- Using_Mbed_Tools/blinky/build_and_flash.sh
+++ Using_Mbed_Tools/blinky/build_and_flash.sh
@@ -5,5 +5,4 @@ cd cmake_build/DISCO_F429ZI/develop/GCC_ARM
 sudo /home/npvinh/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI -c port=SWD sn=0672FF525470514867054915 -w blinky.bin 0x08000000 -v -rst
 cd ../../../../
 clear
-echo "Build and flash complete. Run the following command to test the UART functionality."
 pytest -s test_uart.py
