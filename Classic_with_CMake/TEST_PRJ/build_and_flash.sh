54256d2cf45962c434befc334fa82b9e3511cac6
Hoang Trieu Nguyen <160012376+NguyenHoangTrieu@users.noreply.github.com>
2025-04-21 20:04:45 +0700

    Update build_and_flash.sh

--- Classic_with_CMake/TEST_PRJ/build_and_flash.sh
+++ Classic_with_CMake/TEST_PRJ/build_and_flash.sh
@@ -4,3 +4,6 @@ mkdir build
 cd build
 cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi-gcc.cmake ..
 ninja
+sudo /home/npvinh/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32_Programmer_CLI -c port=SWD sn=0672FF525470514867054915 -w blinky.bin 0x08000000 -v -rst
+cd ..
+pytest -s test_uart.py
